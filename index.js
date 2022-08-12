f986ad9c65cc5ca8ce08bfcb5675922f7255014e
rch20 <reemshalak22@hotmail.com>
2022-08-12 22:59:52 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+const num1 = 62;
+const num2 = 1;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+const num3 = 19;
+const num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 4, 6, 8, 20);
